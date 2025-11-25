2c1e6bf871c1201849d0d0e65b69139190d1bf5f
Doan Manh <doanvanmanh87@gmail.com>
2025-11-25 11:40:11 +0700

    Update site title

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,6 +1,6 @@
 export const AppConfig = {
 	site_name: 'dcaordie.com',
-	title: 'dcaordie | Nhật ký đầu tư cá nhân',
+	title: 'DCAorDie | Nhật ký đầu tư cá nhân',
 	description: 'Chia sẻ quan điểm cá nhân về đầu tư, chọn cổ phiếu, triết lý DCA, mua và nắm giữ dài hạn',
 	author: 'Bro',
 	locale_region: 'vn-vi',
